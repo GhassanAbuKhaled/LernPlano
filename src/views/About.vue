e9c360c8e84777ee8fc5451a989faf64c6079654
Ghassan <ahmedkh12345d@gmail.com>
2023-06-09 00:28:18 +0200

    Added content to 'über uns' page

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,36 @@
   <div class="content text-cenetr">
     <!-- content of the page -->
 
+    <div class="container mt-5">
+    
+    <p>Hi, I'm <strong>Ghassan Abu Khaled</strong>, the programmer and creator of LernPlano. I have a strong passion for web development and have dedicated significant time and effort into designing and building this application.</p>
+    
+    <p>I have a background in computer science and have been working in the software development industry for 3 years. My expertise lies in frontend and backend development, and I love bringing ideas to life through elegant and functional web applications.</p>
+    
+    <p>Throughout the development process, I focused on creating a seamless user experience, implementing robust security measures, and ensuring the application's scalability for future enhancements.</p>
+    
+    <p>I hope you find LernPlano useful and enjoyable to use. If you have any feedback, questions, or suggestions, please don't hesitate to reach out to me through the provided contact information.</p>
+    
+    <p>Thank you for using LernPlano!</p>
+    <p><strong>Email: </strong><a href="mailto:ghassanabokhaled@gmail.com">ghassanabokhaled@gmail.com</a>.</p>
+    <p><strong>github:</strong><a href="https://gist.github.com/GhassanAbuKhaled"><i class="bi bi-github m-2"></i></a></p>
+  </div>
   </div>
 </template>
 <script>
 export default {};
 </script>
+<style scoped>
+i{
+  color: black;
+  cursor: pointer;
+
+}
+a{
+  cursor: pointer;
+}
+i:hover {
+  transform: scale(1.2);
+  color: blue;
+}
+</style>
