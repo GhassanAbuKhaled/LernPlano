13bf44b29eb34c5ce336496e55a2d296728ac09d
GhassanAbuKhaled <93045784+GhassanAbuKhaled@users.noreply.github.com>
2023-12-21 01:27:45 +0100

    Update componentData.js

--- src/store/data/componentData.js
+++ src/store/data/componentData.js
@@ -6,8 +6,28 @@ const state = {
   menuPosition: {},
   Lernziele: null,
   LernPfadName: null,
+  senariosOrder: [], // Inside componentData
+};
+const mutations = {
+  updateOrder: (state, data) => {
+
+    const senariosData = {};
+    Object.entries(state.senariosData).forEach(([key, value], index) => {
+      // Modify the key as needed
+      const updatedKey = `Senario-${index + 1}`;
+      // Store the updated key-value pair in the new object
+      senariosData[updatedKey] =
+        state.senariosData[data.updatedOrder[index].split("?")[1]];
+    });
+    state.senariosData = {};
+
+    setTimeout(() => {
+      state.senariosData = senariosData;
+    }, 50);
+  },
 };
 
 export default {
   state,
+  mutations,
 };
