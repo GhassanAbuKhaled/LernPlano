16357c8c3c6c1b9c6730de303120dc18c448dcdb
GhassanAbuKhaled <93045784+GhassanAbuKhaled@users.noreply.github.com>
2023-12-23 00:48:20 +0100

    Create PageNotFound.vue

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="content p-2 mt-5">
+    <h1>404 - Page Not Found</h1>
+    <p>This page no longer exists or was moved to another location.</p>
+    </div>
+    </template>
+    <script>
+    export default {};
+    </script>
