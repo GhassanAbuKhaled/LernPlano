2ca428f8a61e9d58870a3079d54a7e85039acd4d
GhassanAbuKhaled <93045784+GhassanAbuKhaled@users.noreply.github.com>
2023-12-23 00:44:06 +0100

    Update Datenschutzerklaerung.vue

--- src/views/Datenschutzerklaerung.vue
+++ src/views/Datenschutzerklaerung.vue
@@ -1,9 +1,16 @@
 <template>
 
     <div class="content">
-    
+    <div style="position: relative; margin-top: 5%; width: 70%; height: 70%; left: 15%; right: 15%; text-align: center;">
       <!-- content of the page -->
-     
+      <h1 style="text-align: center; padding: 5%; color:#83bb2c">Subdomainspezifische<br>Datenschutzerklärung</h1>
+      <p>Diese subdomainspezifische Datenschutzerklärung gilt für die Domains:</p>
+
+<a target="_blank" style="color: black;" href="http://www.elp.uni-wuppertal.de/apps/lernpfadplano/">www.elp.uni-wuppertal.de/apps/lernpfadplano</a><br><br>
+
+<p>Die Datenschutzerklärung der Bergischen Universität Wuppertal wird <br> durch die folgenden Ergänzungen erweitert:</p>
+<a target="_blank" style="color: #83bb2c;" href="https://www.uni-wuppertal.de/de/datenschutz"><strong>Datenschutzerklärung der Bergischen Universität Wuppertal</strong></a>
+</div>
     </div>
     </template>
     
